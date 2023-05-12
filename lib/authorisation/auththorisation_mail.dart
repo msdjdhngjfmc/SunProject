7e9f40b79711fd725bafb288930c1f7a4f25db5d
Второй акк <reporty18@gmail.com>
2023-05-12 12:46:04 +0300

    firstscreen authorisation

---
+++ lib/authorisation/auththorisation_mail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class AuthMailScreen extends StatelessWidget {
+  const AuthMailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+    );
+  }
+}
