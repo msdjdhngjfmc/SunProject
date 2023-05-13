ee29335bf798e58dfb5dc294230b8c8df71ed624
Второй акк <reporty18@gmail.com>
2023-05-13 16:52:41 +0300

    fix widgets card and routs

--- lib/menu/booking/booking_buy.dart
+++ lib/menu/booking/booking_buy.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sun_project/menu/booking/routs.dart';
 
 const List<Tab> TabBooking = <Tab>[
   Tab(text: 'Главное'),
@@ -70,7 +71,7 @@ class _BookingMainState extends State<BookingMain> with SingleTickerProviderStat
         controller: controller,
         children: <Widget>[
           BookingBuy(),
-          BookingBuy(),
+          Routs(),
         ],
       ),
     );
@@ -102,8 +103,86 @@ class BookingBuy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('ghb'),
-    );
+    return Expanded(child: ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: const Color(0xFF35B9DE),
+                    width: 1.0,
+                    style: BorderStyle.solid,
+
+                  ),
+                  borderRadius: const BorderRadius.all(
+                      Radius.circular(10.0) //                 <--- border radius here
+                  ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text('Ресторан',style: TextStyle(fontSize: 16,),),
+                              Text('13:30-16:00',style: TextStyle(color: Color(0xFF35B9DE),fontSize: 16),)
+                            ],
+                          ),
+                          Image.asset('images/bahroma.png'),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text('27.03.2022',textAlign: TextAlign.end,style: TextStyle(fontSize: 16),),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              TextButton(
+                                onPressed: () {},
+                                style: TextButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 4),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15.0)),
+                                    side: BorderSide(color: Color(0xFF35B9DE),width: 1)),
+
+                                child: const Text('Оценить', style: TextStyle(color:  Color(0xFF35B9DE),fontSize: 14),),
+                              ),
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              TextButton(
+                                onPressed: () {},
+                                style: TextButton.styleFrom(
+                                  backgroundColor: Color(0xFF35B9DE),
+                                  padding: const EdgeInsets.symmetric(horizontal: 40,vertical: 4),
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                                  side: const BorderSide(color: Color(0xFF35B9DE),width: 1),
+                                ),
+                                child: const Text('Детали бронирования', style: TextStyle(color: Colors.white,fontSize: 14),),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    ));
   }
 }
